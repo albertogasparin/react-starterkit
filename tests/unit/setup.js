66815d0b972f3260ee1b96cc9dabb85c387b14d2
Alberto Gasparin <albertogasparin@gmail.com>
2016-07-28 11:54:35 +0100

    Better test organisation + avoid double loading setup

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,4 +1,4 @@
 // import chai from 'chai';
 
 // This is where you might want to extend chai
-// or set up globals for your tests
+// Remember: mocha env is NOT available here
---
+++ tests/unit/unit.hooks.js
@@ -0,0 +1,3 @@
+/* eslint-env mocha */
+
+// This is where you might want to set up globals hooks
